e5db5b44f94a5f9b104ae768da9e89cb2fddd655
Omanoma <omanoma14@gmail.com>
2023-07-01 21:19:50 +0100

    Finish ValidateCheck Function

--- app/src/main/java/com/example/splash/Login.java
+++ app/src/main/java/com/example/splash/Login.java
@@ -4,10 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -27,24 +30,34 @@ public class Login extends AppCompatActivity {
         db = FirebaseFirestore.getInstance();
         username = findViewById(R.id.login_username);
         password = findViewById(R.id.login_password);
-        add();
-
     }
     private void add(){
         Map<String, Object> users = new HashMap<>();
         User a = new User("one","two","three");
         db.collection("userInfo").document(a.username).set(a.TurnToHash());
     }
-    private void checkValidate(){
+
+    public void checkValidate(View v){
         String user = username.getText().toString();
         String pass = password.getText().toString();
-         db.collection("userInfo").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+         db.collection("userInfo").document(user).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
              @Override
-             public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                 for(QueryDocumentSnapshot doc : task.getResult()){
+             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                 if(task.isSuccessful()){
+                     if(task.getResult().get("Password").equals(pass)){
+                         Toast.makeText(Login.this, "Correct UserName OR Password", Toast.LENGTH_LONG).show();
+                     }
+                     else{
+                         Toast.makeText(Login.this, "Incorrect UserName OR Password", Toast.LENGTH_LONG).show();
 
+                     }
+
+                 }
+                 else{
+                     Toast.makeText(Login.this, "Incorrect UserName OR Password", Toast.LENGTH_LONG).show();
                  }
              }
          });
     }
+
 }
