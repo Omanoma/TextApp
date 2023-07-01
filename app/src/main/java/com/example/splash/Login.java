e84184e8886831733efc50044ac930dd1cd202eb
Omanoma <omanoma14@gmail.com>
2023-07-01 12:16:34 +0100

    connect Database (FireBase)

--- app/src/main/java/com/example/splash/Login.java
+++ app/src/main/java/com/example/splash/Login.java
@@ -3,12 +3,21 @@ package com.example.splash;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
-
+    EditText username;
+    EditText password;
+    FirebaseFirestore db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        db = FirebaseFirestore.getInstance();
+        username = findViewById(R.id.login_username);
+        password = findViewById(R.id.login_password);
+
     }
 }
