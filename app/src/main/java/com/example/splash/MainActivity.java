277d1380f9d91ab2e5e1764e80a7445cf8e13121
Omanoma <omanoma14@gmail.com>
2023-07-01 11:32:59 +0100

    make login activies start Activities automatic once the splash is do

--- app/src/main/java/com/example/splash/MainActivity.java
+++ app/src/main/java/com/example/splash/MainActivity.java
@@ -4,6 +4,7 @@ import static java.lang.Thread.sleep;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -14,20 +15,26 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
     ImageView ImageView;
     Handler handler;
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         handler = new Handler();
+        intent = new Intent(this,Login.class);
         ImageView = findViewById(R.id.face1);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         rAnimation();
         handler.postDelayed(() -> setContentView(R.layout.splash2),3000);
+        handler.postDelayed(() -> changeActivity(),3500);
 
     }
     private void rAnimation(){
         Animation rototeAnimation = AnimationUtils.loadAnimation(this,R.anim.face);
         ImageView.setAnimation(rototeAnimation);
+    }
+    private void changeActivity(){
+
+        startActivity(intent);
     }
 }
