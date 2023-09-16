0b54473978c11fc65c541aafeae979520a9a19c8
Omanoma <omanoma14@gmail.com>
2023-09-16 18:00:45 +0100

    create pop for pfp

---
+++ app/src/main/java/com/example/splash/ProfilePop.java
@@ -0,0 +1,50 @@
+package com.example.splash;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProfilePop#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProfilePop extends DialogFragment {
+
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+    public FirebaseAuth auth;
+
+    public ProfilePop() {
+        auth = FirebaseAuth.getInstance();
+    }
+
+    public static ProfilePop newInstance(String param1, String param2) {
+        ProfilePop fragment = new ProfilePop();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_pop, container, false);
+    }
+}
